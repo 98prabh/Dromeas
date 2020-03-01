5cec43c648fa207eaeabe6c2bdc15028a71ba796
prabh <98prabh@gmail.com>
2020-03-01 15:02:55 +0530

    updated UI

---
+++ app/src/main/java/com/example/dromeas/ImportFromEditor.java
@@ -0,0 +1,14 @@
+package com.example.dromeas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImportFromEditor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_import_from_editor);
+    }
+}
