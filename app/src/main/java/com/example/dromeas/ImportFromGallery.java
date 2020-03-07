7400ce547295d1f1c01f561fe75faf96035c147d
prabh <98prabh@gmail.com>
2020-03-07 16:44:06 +0530

    gallery

--- app/src/main/java/com/example/dromeas/ImportFromGallery.java
+++ app/src/main/java/com/example/dromeas/ImportFromGallery.java
@@ -1,14 +1,97 @@
 package com.example.dromeas;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.sax.StartElementListener;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ImportFromGallery extends AppCompatActivity {
+
+    ImageView image;
+    Button button;
+    private static final  int imagePickCode = 1000;
+    private static final  int permissionCode = 1001;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_import_from_gallery);
+
+        image = findViewById(R.id.galleryimage);
+        button= findViewById(R.id.getimage);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.M)
+                {
+                    if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+                    {
+                        //permission not granted
+                        String[] permission = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        requestPermissions(permission,permissionCode);
+
+
+                    }
+                    else
+                    {
+                        //permission already granted
+                        pickImageFromGallery();
+
+                    }
+                }
+                else
+                {
+                    // os version not compatible
+                    pickImageFromGallery();
+
+                }
+            }
+        });
+    }
+
+    private void pickImageFromGallery()
+    {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, imagePickCode);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode)
+        {
+            case permissionCode:{
+                if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    //permission granted
+                    pickImageFromGallery();
+                }
+                else
+                {
+                    Toast.makeText(this, "Permission Denied",Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode == RESULT_OK && requestCode == imagePickCode)
+        {
+            image.setImageURI(data.getData());
+        }
     }
 }
